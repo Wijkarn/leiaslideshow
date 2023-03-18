60e069e449a17ed5aa25dfd43bbe1883bb1d7346
Wijkarn <89905143+Wijkarn@users.noreply.github.com>
2023-03-18 08:15:05 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,16 +7,40 @@ const img = document.querySelector("img");
 
 carousel();
 
-document.addEventListener("keydown", (event) =>{
-    if(event.key > 0 && event.key < 10){
+document.addEventListener("keydown", (event) => {
+    // console.log(event.key)
+
+    if (event.key > 0 && event.key < 10) {
         seconds = event.key;
-        if(!run){
+        if (!run) {
             run = true;
             carousel();
         }
     }
-    else if(event.key == 0){
-        run = false;
+    else if (event.key == 0 || event.key == " ") {
+        if (run) run = false;
+        else {
+            run = true;
+            carousel();
+        }
+    }
+    else if (event.key == "ArrowLeft") {
+        if (i > 1) {
+            i--;
+        }
+        else {
+            i = images;
+        }
+        displayImage(i);
+    }
+    else if (event.key == "ArrowRight") {
+        if (i < images) {
+            i++;
+        }
+        else {
+            i = 1;
+        }
+        displayImage(i);
     }
 });
 
@@ -24,9 +48,13 @@ function carousel() {
     if (i < images) {
         i++;
     }
-    else{
+    else {
         i = 1;
     }
+    displayImage(i);
+    if (run) setTimeout(carousel, seconds * 1000);
+}
+
+function displayImage(i) {
     img.src = `./images/leia (${i}).JPG`;
-    if(run) setTimeout(carousel, seconds * 1000); 
 }
